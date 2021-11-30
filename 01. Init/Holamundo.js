a357eb7efcb29bfb4a10b6e34f824be5bc04ca88
Juan Otero <juan.oterol@gmail.com>
2021-11-30 10:58:07 +0100

    Mundo nuevo

--- 01. Init/Holamundo.js
+++ 01. Init/Holamundo.js
@@ -1,8 +1,8 @@
 function holamundo(){
      valor1="Hola";
-     valor2=" mundo3   "
+     valor2=" mundo "
 
-    console.log(valor1+valor2);
+    console.log(valor1+valor2+"nuevo");
 }
 
 holamundo();
